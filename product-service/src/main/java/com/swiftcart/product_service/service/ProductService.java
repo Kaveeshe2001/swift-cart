b86d5127c323d5ed0cd4bb62da49618b7e5e7470
Kaveesha <kaveeshewaduge111@gmail.com>
2026-02-12 18:15:23 +0530

    Product request and response dtos

--- product-service/src/main/java/com/swiftcart/product_service/service/ProductService.java
+++ product-service/src/main/java/com/swiftcart/product_service/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.swiftcart.product_service.service;
 
+import com.swiftcart.product_service.model.Product;
+import com.swiftcart.product_service.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
 }
