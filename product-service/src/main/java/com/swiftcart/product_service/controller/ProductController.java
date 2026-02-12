dc939e05d564afa92afccfd969495df85bb12b42
Kaveesha <kaveeshewaduge111@gmail.com>
2026-02-12 19:19:52 +0530

    productController

--- product-service/src/main/java/com/swiftcart/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/swiftcart/product_service/controller/ProductController.java
@@ -1,4 +1,50 @@
 package com.swiftcart.product_service.controller;
 
+import com.swiftcart.product_service.dtos.ProductRequest;
+import com.swiftcart.product_service.dtos.ProductResponse;
+import com.swiftcart.product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        return new ResponseEntity<>(productService.createProduct(productRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+        ProductResponse updateProduct = productService.updateProduct(id, productRequest);
+        return ResponseEntity.ok(updateProduct);
+    }
 }
