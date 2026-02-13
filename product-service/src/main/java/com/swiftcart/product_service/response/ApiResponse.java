9e088135a0c1c06c5ddc485b66d235dcf86b2e4f
Kaveesha <kaveeshewaduge111@gmail.com>
2026-02-13 14:18:04 +0530

    make final change

--- product-service/src/main/java/com/swiftcart/product_service/response/ApiResponse.java
+++ product-service/src/main/java/com/swiftcart/product_service/response/ApiResponse.java
@@ -1,11 +1,9 @@
 package com.swiftcart.product_service.response;
 
 import com.swiftcart.product_service.dtos.ProductResponse;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 // Every single request returns the exact same format.
-@AllArgsConstructor // generates a constructor that takes both arguments
 @Data // automatically generates Getters, Setters, toString(), equals(), and hashCode()
 public class ApiResponse {
     private String message; // holds a human-readable status message
